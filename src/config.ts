fb3c15bbd3beacb865e50444aac9028fc5c408b3
kakaka820 <tata_ta@163.com>
2025-10-08 18:13:26 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,14 @@
 // src/config.ts
 
+
+//待機時間設定
 export const WAIT_TIME_MS = 1500;
+
+
+
+//アンティ
+export const ANTE = 100;
+//JOKERが何回出たら終了か
+export const MAX_JOKER_COUNT = 1;
+//この得点以下になったら終了
+export const MIN_POINTS = 0;
