6f1b2b1002853dd3f4c525356d306d55d7569320
kakaka820 <tata_ta@163.com>
2025-10-09 00:36:09 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './App';
 import './index.css'; // CSSを使ってる場合
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
