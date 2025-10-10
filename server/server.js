de2235678c5c8d34d67bdcec3bf3671a9462810f
kakaka820 <tata_ta@163.com>
2025-10-10 19:48:08 +0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -233,7 +233,10 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
