aa6bf1bd106fa047882c75636d14ff2792f2b747
kakaka820 <tata_ta@163.com>
2025-10-12 18:35:18 +0800

    Add files via upload

--- server/gameManager.js
+++ server/gameManager.js
@@ -1,8 +1,15 @@
 // server/gameManager.js
 // ゲーム管理の統括（エントリーポイント）
 
-const { startGame } = require('./gameStarter');
+const { startGame: startGameCore } = require('./gameStarter');
 const { handleRoundEnd } = require('./roundHandler');
+
+/**
+ * ゲーム開始（handleRoundEndを自動注入）
+ */
+function startGame(io, games, roomId, room) {
+  startGameCore(io, games, roomId, room, handleRoundEnd);
+}
 
 module.exports = {
   startGame,
