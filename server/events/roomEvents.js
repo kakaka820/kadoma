7b3100753a66a30272c69ae3aeb4c2b3aeff8759
kakaka820 <tata_ta@163.com>
2025-11-20 14:29:34 +0800

    Add files via upload

--- server/events/roomEvents.js
+++ server/events/roomEvents.js
@@ -3,7 +3,13 @@
 
 const { handleJoinRoom } = require('../roomManager');
 const { startGame } = require('../gameManager');
-const { handleMultiRoomJoin } = require('../utils/multiRoomHandler'); // ← 新規作成
+const { handleMultiRoomJoin } = require('../utils/multiRoomHandler');
+const { 
+  handleCreateCustomRoom,
+  handleGetCustomRooms,
+  handleJoinCustomRoom 
+} = require('../utils/customRoomHandler');
+
 
 function setupRoomEvents(socket, io, rooms, games) {
   // 通常ルーム参加
@@ -22,6 +28,25 @@ function setupRoomEvents(socket, io, rooms, games) {
   socket.on('join_multi_room', async (data, callback) => {
     await handleMultiRoomJoin(socket, io, rooms, games, data, callback);
   });
+
+  
+ // カスタム部屋作成
+  socket.on('create_custom_room', async (data, callback) => {
+    await handleCreateCustomRoom(socket, io, rooms, data, callback);
+  });
+  
+  // カスタム部屋一覧取得
+  socket.on('get_custom_rooms', (callback) => {
+    handleGetCustomRooms(rooms, callback);
+  });
+
+  
+  // カスタム部屋参加
+  socket.on('join_custom_room', async (data, callback) => {
+    await handleJoinCustomRoom(socket, io, rooms, games, data, callback);
+  });
+
+
   
   // マッチングキャンセル
   socket.on('cancel_matching', async (data, callback) => {
